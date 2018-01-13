c70be63341d19a94468b1e0c1d5c3b69fcac1734
Brian Patrick <batpat772@gmail.com>
2018-01-13 14:02:05 -0800

    added GroceryList.js

---
+++ client/src/pages/GroceryList.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { BrowswerRouter as Router, Route, Switch } from "react-router-dom";
+
+import API from "../api";
+
+class GroceryList extends Component {
+   render() {
+      return (
+         
+      );
+   }
+}
