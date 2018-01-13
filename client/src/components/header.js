d9b48a31367efaa36fcd0cfaae61279c171f8783
Chelsey Dolan <chelseymarie6@gmail.com>
2018-01-13 14:10:12 -0800

    updates to components

---
+++ client/src/components/header.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+
+import API from "../api";
+
+class Header extends Component {
+  render() {
+    return (
+        <div id="page-transitions">
+              
+        <div className="header header-dark">
+            <a href="#" className="header-icon header-icon-1 open-left-sidebar"><i className="fa fa-navicon"></i></a>
+            <a href="index.html" className="header-logo scale-hover"></a>
+            <a href="#" className="header-icon header-icon-4 open-right-sidebar"><i className="fa fa-shopping-cart"></i></a>
+        </div>
+        );
+    }
+}
+
+export default Header
+
+
+     
