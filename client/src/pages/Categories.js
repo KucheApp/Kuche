068e313303412277451233e418a25eedd2b19658
Brian Patrick <batpat772@gmail.com>
2018-01-17 22:22:13 -0800

    added Categories which will use material-ui's grid system with Cards.js for the kitchen page

---
+++ client/src/pages/Categories.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { withStyles } from 'material-ui/styles';
+import Paper from 'material-ui/Paper';
+import Grid from 'material-ui/Grid';
+import Cards from './Cards';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    marginTop: 150,
+  },
+  paper: {
+    padding: 16,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
+
+class Categories extends Component {
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <Grid container spacing={24}>
+          <Grid item xs={6} sm={3}>
+            <Paper className={classes.paper}>
+              <Cards />
+            </Paper>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Paper className={classes.paper}>
+              <Cards />
+            </Paper>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Paper className={classes.paper}>
+              <Cards />
+            </Paper>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Paper className={classes.paper}>
+              <Cards />
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+
+export default withStyles(styles)(Categories);
