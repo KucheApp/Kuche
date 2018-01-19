c65d7a50a3d5cb0cc02763878aadd2030ca09a2a
Chelsey Dolan <chelseymarie6@gmail.com>
2018-01-18 21:02:00 -0800

    added form and close in accordian.js

--- client/src/pages/Accordion.js
+++ client/src/pages/Accordion.js
@@ -6,13 +6,26 @@ class Accordion extends Component {
 
     render() {
       return (
-        <div className="container">
+      <div className="container">
+          <form>
+            <div className="form-group row">
+              <label for="input" className="col-sm-2 col-form-label">Text</label>
+              <div className="col-sm-10">
+                <input type="text" className="form-control" id="input" placeholder="Add food" />
+                <button type="submit" className="btn-primary">Add</button>
+              </div>
+            </div>
+          </form>
+
         <div className="row">
           <div className="col-2"></div>
           <div className="col-8">
             <div id="accordion" role="tablist" aria-multiselectable="true">
               <div className="card">
                 <div className="card-header" role="tab" id="headingOne">
+                  <button type="button" className="close" aria-label="Close" position="right">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
                   <h5 className="mb-0">
                     <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                       Bananas
@@ -28,6 +41,9 @@ class Accordion extends Component {
               </div>
               <div className="card">
                 <div className="card-header" role="tab" id="headingTwo">
+                  <button type="button" className="close" aria-label="Close" position="right">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
                   <h5 className="mb-0">
                     <a className="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                       Bread
@@ -42,6 +58,9 @@ class Accordion extends Component {
               </div>
               <div className="card">
                 <div className="card-header" role="tab" id="headingThree">
+                  <button type="button" className="close" aria-label="Close" position="right">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
                   <h5 className="mb-0">
                     <a className="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                       Onions
