a2ba83bdd980c4487d95ab7643922c35ccd76419
Chelsey Dolan <chelseymarie6@gmail.com>
2018-01-23 18:48:07 -0800

    modal

--- client/src/pages/Modal.js
+++ client/src/pages/Modal.js
@@ -30,90 +30,63 @@ class ModalAdd extends Component {
           <ModalHeader toggle={this.toggle}>Add Food</ModalHeader>
           <ModalBody>
             <Form>
+
                 <FormGroup>
-                    <Label for="exampleEmail">Plain Text (Static)</Label>
-                    <Input plaintext>Some plain text/ static value</Input>
+                    <Label for="foodName">Name: </Label>
+                    <Input type="email" name="food" placeholder="add a food" />
                 </FormGroup>
+ 
                 <FormGroup>
-                    <Label for="exampleEmail">Email</Label>
-                    <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="examplePassword">Password</Label>
-                    <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleUrl">Url</Label>
-                    <Input type="url" name="url" id="exampleUrl" placeholder="url placeholder" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleNumber">Number</Label>
-                    <Input type="number" name="number" id="exampleNumber" placeholder="number placeholder" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleDatetime">Datetime</Label>
-                    <Input type="datetime" name="datetime" id="exampleDatetime" placeholder="datetime placeholder" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleDate">Date</Label>
-                    <Input type="date" name="date" id="exampleDate" placeholder="date placeholder" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleTime">Time</Label>
-                    <Input type="time" name="time" id="exampleTime" placeholder="time placeholder" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleColor">Color</Label>
-                    <Input type="color" name="color" id="exampleColor" placeholder="color placeholder" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleSearch">Search</Label>
-                    <Input type="search" name="search" id="exampleSearch" placeholder="search placeholder" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleSelect">Select</Label>
-                    <Input type="select" name="select" id="exampleSelect">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
+                    <Label for="exampleSelect">Category</Label>
+                    <Input type="select" name="selectCategory" id="exampleSelect">
+                        <option>Counter</option>
+                        <option>Fridge</option>
+                        <option>Freezer</option>
+                        <option>Pantry</option>
                     </Input>
                 </FormGroup>
+
                 <FormGroup>
-                    <Label for="exampleSelectMulti">Select Multiple</Label>
-                    <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
+                    <Label for="exampleNumber">Quantity</Label>
+                    <Input type="number" name="number" id="exampleNumber" placeholder="quantity" />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label for="exampleSelect">Units</Label>
+                    <Input type="select" name="select" id="">
+                        <option>pound</option>
+                        <option>quart</option>
+                        <option>gallon</option>
+                        <option>pint</option>
+                        <option>cup</option>
+                        <option>ounce</option>
+                        <option>tbsp</option>
+                        <option>tsp</option>
+                        <option>package</option>
+
                     </Input>
                 </FormGroup>
+
                 <FormGroup>
-                    <Label for="exampleText">Text Area</Label>
-                    <Input type="textarea" name="text" id="exampleText" />
+                    <Label for="exampleDate">Date Purchased</Label>
+                    <Input type="date" name="date" id="exampleDate" placeholder="date purchased" />
                 </FormGroup>
+
                 <FormGroup>
-                    <Label for="exampleFile">File</Label>
+                    <Label for="exampleDate">Date Expires</Label>
+                    <Input type="date" name="date" id="exampleDate" placeholder="date expires" />
+                </FormGroup>
+
+
+               
+                <FormGroup>
+                    <Label for="exampleFile">Choose a File</Label>
                     <Input type="file" name="file" id="exampleFile" />
                     <FormText color="muted">
-                        This is some placeholder block-level help text for the above input.
-                        It's a bit lighter and easily wraps to a new line.
+                        Should we have an option to upload anything?
                 </FormText>
                 </FormGroup>
-                <FormGroup check>
-                    <Label check>
-                        <Input type="radio" />{' '}
-                        Option one is this and that—be sure to include why it's great
-                    </Label>
-                </FormGroup>
-                <FormGroup check>
-                    <Label check>
-                        <Input type="checkbox" />{' '}
-                        Check me out
-                    </Label>
-                </FormGroup>
+
             </Form>
           </ModalBody>
           <ModalFooter>
